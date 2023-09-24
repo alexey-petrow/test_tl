d77c1feb9cbe9c27ee39aef0f6bbed7d670a9b74
Aleksey <alexeypetrow21@gmail.com>
2023-09-24 17:37:43 +0300

    create models and make basic config of admin zone

--- django_app/staff/admin.py
+++ django_app/staff/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from staff.models import Department, Employee
+
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'parent', 'employees_count']
+    list_filter = ['parent']
+    list_per_page = 20
+    search_fields = ['name']
+
+    @admin.display(description='number of employees')
+    def employees_count(self, obj):
+        return obj.employees.count()
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'position',
+                    'hire_date', 'salary', 'department']
+    list_filter = ['position', 'department']
+    list_per_page = 50
+    list_select_related = ['department']
+    search_fields = ['first_name', 'last_name', 'middle_name']
+
+
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Employee, EmployeeAdmin)
