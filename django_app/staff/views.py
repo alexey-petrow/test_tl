8fa07cb40ce691bee79774bf46032e2243152e7e
Aleksey <alexeypetrow21@gmail.com>
2023-09-25 10:08:30 +0300

    create filling with test data

--- django_app/staff/views.py
+++ django_app/staff/views.py
@@ -6,3 +6,4 @@ class DepartmentsTreeView(ListView):
     template_name = 'department_tree.html'
     queryset = Department.objects.filter(parent__isnull=True)
     context_object_name = 'departments'
+    extra_context = {'title': 'Departments tree'}
